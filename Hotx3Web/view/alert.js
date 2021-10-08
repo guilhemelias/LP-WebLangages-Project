90163760e23d92bbd41d4b77cc0b2139bbbfe71f
guilhemelias <guilhem.elias@gmail.com>
2021-10-08 17:30:28 +0200

    Graphic part
    Implementation of the graphic part with chart js. Next step : responsive part

--- Hotx3Web/view/alert.js
+++ Hotx3Web/view/alert.js
@@ -125,3 +125,46 @@ function tempToWeight(temp) {
 
     return temp * 0.02857
 }
+
+
+
+
+
+
+
+var myChart = new Chart(document.getElementById("line-chart"), {
+    type: 'line',
+    data: {
+        labels: ["M", "Tu", "W", "Th", "F", "Sa", "Su"],
+        datasets: [{
+            data: [30, 14, 15, 18, 27, 22, 21],
+            label: "Inside",
+            borderColor: "#3e95cd",
+            fill: false
+        }, {
+            data: [7, -3, 30, 47, 45, 11, 14],
+            label: "Outside",
+            borderColor: "#8e5ea2",
+            fill: false
+        }]
+    },
+    options: {
+        legend: {
+            position: 'bottom'
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    display: false
+                }
+            }],
+            yAxes: [{
+                gridLines: {
+                    display: false
+                }
+            }]
+        }
+    }
+
+
+});
